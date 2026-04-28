3b4eb8a0bd2c9d1ba53afae6e335a5f85e88fe4c
felipeliralira <felipe.lira.oliveira@escola.pr.gov.br>
2026-04-28 08:15:44 -0300

    Add files via upload

---
+++ avaliação 2.sql
@@ -0,0 +1,9 @@
+use sucos_vendas;
+
+select * from itens_notas_fiscais;
+# Quantidade
+select * from notas_fiscais;
+select * from tabela_de_produtos;
+# Sabor
+
+select distinct sabor from tabela_de_produtos;
